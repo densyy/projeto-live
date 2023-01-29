1d15db7c2406a5b0feecc9fea2f118f350bc4c84
Densyy <densyy@hotmail.com>
2023-01-28 21:16:18 -0300

    criação das rotas

--- app.js
+++ app.js
@@ -27,3 +27,53 @@ function deuErro (erro) {
 function taRodando () {
   console.log('Já estou capturando os trouxas')
 }
+
+/* ---- Rotas ---- */
+
+app.use(express.json())
+
+// GET - Receber
+// POST - Adicionar
+// PUT - Edições
+// DELETE - Deletar
+// PATCH - Edições Pontuais
+
+app.get('/bem-vindo', bemVindo)
+
+function bemVindo (_request, response) {
+
+  // 200 201 - SUCESSO
+  // 400 401 402 403 404 406 -- ERRO USUARIO
+  // 500 501 ... ERRO DO SERVIDOR
+
+  response
+    .status(200)
+    .json('Bem vindo viu maluco!! Tô feliz por ter você aqui!')
+}
+
+/* ---- Routes - RECEBER USUARIO ---- */
+
+app.get('/usuarios', receberUsuarios)
+
+function receberUsuarios (_request, response) {
+  const usuarios = require('./usuarios.json')
+
+  response
+    .status(200)
+    .json(usuarios)
+}
+
+/* ---- Routes - ADICIONAR USUARIO ---- */
+
+app.post('/usuarios', adicionarUsuario)
+
+function adicionarUsuario (request, response) {
+  const dados = request.body
+
+  const email = dados.email
+  const nome = dados.nome
+
+  response
+    .status(200)
+    .json(`email: ${email} - nome: ${nome}`)
+}
